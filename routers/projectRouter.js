139be98b36c9fad18f65eb794139561ba1ff5a3d
May Ng <mngmay@gmail.com>
2019-09-06 09:14:18 -0700

    PUT project created

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -50,8 +50,31 @@ router.delete("/:id", validateProjectId, (req, res) => {
   Projects.remove(id)
     .then(project => res.status(200).json(project))
     .catch(error =>
-      res.status(500).json({ error: "The project could not be deleted" })
+      res.status(500).json({ error: "The project could not be deleted." })
     );
+});
+
+// PUT/update a project
+
+router.put("/:id", validateProjectId, (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  if (Object.keys(changes) < 1) {
+    return res.status(400).json({ error: "Missing property data" });
+  }
+
+  if (changes.name || changes.description || changes.description) {
+    Projects.update(id, changes)
+      .then(updated => res.status(200).json(updated))
+      .catch(error =>
+        res.status(500).json({ error: "The project could not be updated." })
+      );
+  } else {
+    return res
+      .status(400)
+      .json({ error: "Please provide a valid property to update" });
+  }
 });
 
 // custom middleware
